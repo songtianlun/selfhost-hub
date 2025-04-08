f87bec6b88995cb29cba13869ae179cca9554504
songtianlun <tianlun.song@foxmail.com>
2025-04-08 13:11:53 +0800

    feat: update site information and add localization
    - Change site URL to 'https://selfhost-hub.com'
    - Update project title to 'SelfHost-Hub'
    - Modify GitHub social link to point to the correct repo
    - Add localization support for English and Simplified Chinese
    
    These updates enhance the project by providing accurate site details and
    supporting multiple languages, improving accessibility for users.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://selfhost-hub.com',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'SelfHost-Hub',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/songtianlun/selfhost-hub' }],
 			sidebar: [
 				{
 					label: 'Guides',
@@ -21,6 +22,18 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en', // lang 是 root 语言必须的
+				  },
+				// 简体中文文档在 `src/content/docs/zh-cn/` 中。
+				'zh-cn': {
+				  label: '简体中文',
+				  lang: 'zh-CN',
+				},
+			  },
 		}),
 	],
 });
